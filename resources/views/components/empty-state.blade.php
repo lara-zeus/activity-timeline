e2bb2c9a29deaf435bfdc4af4849134143f34651
Ash Monsh <wh7r.com@gmail.com>
2026-07-24 19:15:30 +0300

    add compact option

--- resources/views/components/empty-state.blade.php
+++ resources/views/components/empty-state.blade.php
@@ -1,10 +1,15 @@
 @props([
+    'compact' => false,
     'description' => null,
     'heading',
     'icon',
 ])
 
-<div {{ $attributes->class(['fi-timeline-empty-state px-6 py-12']) }}>
+<div {{ $attributes->class([
+    'fi-timeline-empty-state',
+    'px-6 py-12' => ! $compact,
+    'py-6' => $compact,
+]) }}>
     <div class="grid max-w-lg mx-auto text-center fi-timeline-empty-state-content justify-items-center">
         <div class="p-3 mb-4 bg-gray-100 rounded-full fi-timeline-empty-state-icon-ctn dark:bg-gray-500/20">
             <x-filament::icon :icon="$icon"
